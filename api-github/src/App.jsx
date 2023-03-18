3151a13a12dd465bf7ce76ebbb427e1e1bc424a7
Erick Gabriel <eg654099@gmail.com>
2023-03-18 18:26:51 -0300

    Initial commit

---
+++ api-github/src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <header>
+        <h1>Busque Repositórios no GitHub</h1>
+      </header>
+      <main>
+
+      </main>
+      <footer>
+        <p>Desenvolvido por <a href='https://github.com/EGBDS' target='_blank'>EGBDS</a></p>
+      </footer>
+    </div>
+  )
+}
+
+export default App
