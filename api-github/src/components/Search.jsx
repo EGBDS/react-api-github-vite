c32474572bd0291d0a980dbad2ec74226e16d261
Erick Gabriel <eg654099@gmail.com>
2023-03-27 20:02:20 -0300

    buttons 100% configurados.

--- api-github/src/components/Search.jsx
+++ api-github/src/components/Search.jsx
@@ -8,7 +8,9 @@ import './Search.css';
 
 const Search = ()  => {
 
-    const sleep = ms => new Promise(res => setTimeout(res, ms));
+    const sleep = ms => new Promise(res => setTimeout(res, ms)); //configuração padrão para o sleep
+
+    const [ qtdp, setQtdp ] = useState(0);
 
     const [ qtd, setQtd ] = useState(0);
     
@@ -22,26 +24,34 @@ const Search = ()  => {
 
     const [ pag_range_end, setPage_Range_End ] = useState(7);
 
-    
+    const [ marq, setMarq ] = useState(0);
 
     const getdados = async () => { // funcao assincrona
         try { 
             const resposta = await url_default.get(repo+`/page=${pag}`); //tentando fazer a requisição
             const dado = resposta.data.items;
             const quant =  resposta.data.total_count;
+            
+            setQtd(quant);
 
-            if(qtd > quant) {
-                setQtd(qtd);
+            if(qtdp > quant) {
+                setQtdp(qtdp);
             }
             else {
-                setQtd(quant);
+                setQtdp(quant);
             };  
-            if(dado.length <= 1){
+            if(dado.length < 1){
                 let dad = document.getElementById('dad');
                 dad.innerHTML = `<p>Carregando...</p>`;
 
                 let dados = document.getElementById('dados');
                 dados.style.display = "none";
+
+                let paginate = document.getElementById('paginate');
+                paginate.style.display = "none";
+
+                let qtd = document.getElementById('qtd');
+                qtd.style.display = "none";
 
                 await sleep(4000);
                 dad.innerHTML = `<p>Repositório não encontrado! Tente novamente.</p>`;
@@ -51,16 +61,22 @@ const Search = ()  => {
                 let dados = document.getElementById('dados');
                 dados.style.display = "block";
 
+                let paginate = document.getElementById('paginate');
+                paginate.style.display = "block";
+
                 let dad = document.getElementById('dad');
                 dad.innerHTML = ``;
-            }
+
+                let qtd = document.getElementById('qtd');
+                qtd.style.display = "block";
+            };
             
             console.log(dado);
         } catch(error) {
             console.log(error);
 
             if( repo != '') { 
-                alert("Há algo de errado com a API. Recarregue a página. Se não funcionar tente outra hora, ou entre em contato com os desenvolvedores!"+error)
+                alert("Há algo de errado com a API. Recarregue a página. Se não funcionar tente outra hora, ou entre em contato com os desenvolvedores! Tente novamente em alguns minutos. "+error)
             };
         }
         
@@ -70,17 +86,20 @@ const Search = ()  => {
         getdados();
     },[pag]);
 
+    var last = paginas().length - 1
+
     function paginas() {
 
         let pages = [];
-        /* console.log(pages) */
+        
+
         let a = 0;
         pages.unshift(
-            <a href="#header" key={a.toString()}>
-                <button onClick={ first_page }>First</button>
+            <a href="#header" key={a.toString()} >
+                <button onClick={ first_page } id='0'>First</button>
             </a>
         );
-        for (let i = 1; i < (qtd/31); i++){
+        for (let i = 1; i < (qtdp/31); i++){
             pages.push(
             <a href="#header" key={i.toString()}>
                 <button  id={i.toString()} onClick={any_page}>{i}</button>
@@ -91,7 +110,7 @@ const Search = ()  => {
         };
         pages.push(
             <a href="#header" key={last_page}>
-                <button onClick={ last_page }>Last</button>
+                <button onClick={ last_page } id={last}>Last</button>
             </a>
         );
         return pages;
@@ -112,24 +131,43 @@ const Search = ()  => {
         return last;
     };
 
-    console.log(paginas().length)
-
     function any_page() {
         
         document.querySelectorAll("button").forEach( function(button) {
             button.addEventListener("click", function(event) {
             const el = event.target;
             const id = el.id;
+            
+            let reset = document.getElementById(marq);
+            let foco = document.getElementById(id);
+
+           
+            reset.style.background = "white";
+            reset.style.color = "black";
+            reset.style.border = "1px solid black";
+            
+            foco.style.background = "gray";
+            foco.style.color = "white";
+
+            setMarq(id);
             setPag(id);
+
             if(id > 1){
                 setPage_Range_First(id);
+
                 if(id < paginas().length - 2){
-                    setPage_Range_First(id);
-                    setPage_Range_End(parseInt(id) + 7);
+                    if(id >= 6) {
+                        setPage_Range_First(id - 3);
+                        setPage_Range_End(parseInt(id) + 3);
+                    }
+                    else {
+                        setPage_Range_First(1);
+                        setPage_Range_End(7);
+                    }
                 }
                 else {
                     setPage_Range_First(parseInt(id) - 7);
-                    setPage_Range_End(paginas().length - 2)
+                    setPage_Range_End(paginas().length - 2);
                 }
             }
             else {
@@ -142,14 +180,25 @@ const Search = ()  => {
     };
 
 
+
 return (
     <div className='search'>
         <form onSubmit={(e) => {e.preventDefault()}}>
             <input type='text' id='repository' placeholder='Digite Aqui' onChange={(e) => setRepo(e.target.value)}/>
             <button type='submit' id='btn' value='buscar' onClick={getdados}>Buscar</button>
         </form>
-        <div className='qtd'>
+        <div id='qtd'>
             <h2>Quantidade de repositórios: {qtd.toLocaleString()}</h2> {/* tolocalestring faz com que o número tenha pontos para diferenciar de dezena, centena e etc... */}
+        </div>
+        <div id='paginate'>
+            
+            <ul href="#heade" id='paginas'>
+                {paginas().slice(0, 1)}
+                {paginas().slice( pag_range_first, pag_range_end )}
+                {paginas().slice(paginas().length-1 , paginas().length)}
+            </ul>
+            
+            
         </div>
         <div id='dad'>
         </div>
@@ -172,7 +221,7 @@ return (
                 </div>
             )))}
         </div>
-        <div id='paginate'>
+        {/* <div id='paginate'>
             
             <ul href="#header">
                 {paginas().slice(0, 1)}
@@ -181,7 +230,7 @@ return (
             </ul>
             
             
-        </div>
+        </div> */}
     </div>
 )
 };
