28fe4f48c15255231514d76728d2c5122dcc1c64
Erick Gabriel <eg654099@gmail.com>
2023-03-31 18:22:08 -0300

    correção na mensagem de carregando ao pesquisar repositórios

--- api-github/src/components/Search.jsx
+++ api-github/src/components/Search.jsx
@@ -8,8 +8,137 @@ import './Search.css';
 
 const Search = ()  => {
 
+    const sleep = ms => new Promise(res => setTimeout(res, ms)); //configuração padrão para o sleep
+
+    /* const [ qtdp, setQtdp ] = useState(0); */
+    
+    const [ qtd, setQtd ] = useState(0);
+    console.log(qtd);
+    
+    const [ dados, setDatos ] = useState([]); //usestate recebe um array
+    console.log(dados);
+    /* const [ repo, setRepo ] = useState(''); //nome do repositorio */
+
+    const [ pag, setPag ] = useState(1);
+    console.log(pag);
+    const [ pag_range_first, setPage_Range_First ] = useState(1);
+
+    const [ pag_range_end, setPage_Range_End ] = useState(7);
+
+    const [ marq, setMarq ] = useState(0);
+
+    const repos = useRef('');
+    console.log(repos.current)
+
+    const getdados = async () => { // funcao assincrona
+        try { 
+            const resposta = await url_default.get(repos.current+`&per_page=10&page=${pag}`); //tentando fazer a requisição.
+            const dado = resposta.data.items;
+            const quant =  resposta.data.total_count;
+            
+            setQtd(quant);
+
+            if(repos.current != '') {
+                if(dado.length < 1){
+                    let dad = document.getElementById('dad');
+                    dad.innerHTML = `<p>Carregando...</p>`;
+
+                    let dados = document.getElementById('dados');
+                    dados.style.display = "none";
+
+                    let paginate = document.getElementById('paginate');
+                    paginate.style.display = "none";
+
+                    let qtd = document.getElementById('qtd');
+                    qtd.style.display = "none";
+
+                    await sleep(4000);
+                    dad.innerHTML = `<p>Repositório não encontrado! Tente novamente.</p>`;
+                    }
+                else {
+                    let dados = document.getElementById('dados');
+                    dados.style.display = "none";
+
+                    let paginate = document.getElementById('paginate');
+                    paginate.style.display = "none";
+
+                    let qtd = document.getElementById('qtd');
+                    qtd.style.display = "none";
+
+                    setDatos(dado);
+                    let dad = document.getElementById('dad');
+                    dad.innerHTML = `<p>Carregando...</p>`;
+                    await sleep(4000);
+                    dad.innerHTML = ``;
+
+                    dados.style.display = "block";
+
+                    paginate.style.display = "block";
+
+                    qtd.style.display = "block";
+                };
+            };
+            
+            console.log(dado);
+        } catch(error) {
+            console.log(error);
+
+            if( repos != '') { 
+                alert("Há algo de errado com a API. Recarregue a página. Se não funcionar tente em alguns minutos ou entre em contato com os desenvolvedores @erick_gbs ! Tente novamente em alguns minutos. "+error);
+            };
+        }
+        
+    };
+
+    useEffect(() => {
+        getdados();
+    },[addEventListener]);
+
+    var last = paginas().length - 1
+
+    function paginas() {
+
+        let pages = [];
+        
+        let a = 0;
+        pages.unshift(
+            <a href="#header" key={a.toString()} >
+                <button onClick={ first_page } id='0'>First</button>
+            </a>
+        );
+        for (let i = 1; i < (qtd/10); i++){
+            pages.push(
+            <a href="#header" key={i.toString()}>
+                <button  id={i.toString()} onClick={any_page}>{i}</button>
+            </a>
+            );
+            
+            
+        };
+        pages.push(
+            <a href="#header" key={last_page}>
+                <button onClick={ last_page } id={last}>Last</button>
+            </a>
+        );
+        return pages;
+       
+    };
+
+    function first_page() {
+        setPag(1);
+        setPage_Range_First(1);
+        setPage_Range_End(7);
+    };
+    
+    function last_page() {
+        
+        setPag(last);
+        setPage_Range_First(paginas().length - 10);
+        setPage_Range_End(paginas().length - 2);
+        return last;
+    };
+
     function any_page() {
-        console.log("any_page")
         document.querySelectorAll("button").forEach( function(button) {
             button.addEventListener("click", function(event) {
             const el = event.target;
@@ -60,143 +189,8 @@ const Search = ()  => {
           
         });
     };
-
-    const sleep = ms => new Promise(res => setTimeout(res, ms)); //configuração padrão para o sleep
-
-    /* const [ qtdp, setQtdp ] = useState(0); */
     
-    const [ qtd, setQtd ] = useState(0);
-    console.log(qtd);
-    
-    const [ dados, setDatos ] = useState([]); //usestate recebe um array
-    console.log(dados);
-    /* const [ repo, setRepo ] = useState(''); //nome do repositorio */
 
-    const [ pag, setPag ] = useState(1);
-    console.log(pag);
-    const [ pag_range_first, setPage_Range_First ] = useState(1);
-
-    const [ pag_range_end, setPage_Range_End ] = useState(7);
-
-    const [ marq, setMarq ] = useState(0);
-
-    const repos = useRef('');
-    console.log(repos);
-    const getdados = async () => { // funcao assincrona
-        try { 
-            const resposta = await url_default.get(repos.current+`&per_page=10&page=${pag}`); //tentando fazer a requisição.
-            const dado = resposta.data.items;
-            const quant =  resposta.data.total_count;
-            
-            setQtd(quant);
-
-            /* if(qtdp > quant) {
-                setQtdp(qtdp);
-            }
-            else {
-                setQtdp(quant);
-            };   */
-
-            if(dado.length < 1){
-                let dad = document.getElementById('dad');
-                dad.innerHTML = `<p>Carregando...</p>`;
-
-                let dados = document.getElementById('dados');
-                dados.style.display = "none";
-
-                let paginate = document.getElementById('paginate');
-                paginate.style.display = "none";
-
-                let qtd = document.getElementById('qtd');
-                qtd.style.display = "none";
-
-                await sleep(4000);
-                dad.innerHTML = `<p>Repositório não encontrado! Tente novamente.</p>`;
-                }
-            else {
-                let dados = document.getElementById('dados');
-                dados.style.display = "none";
-
-                let paginate = document.getElementById('paginate');
-                paginate.style.display = "none";
-
-                let qtd = document.getElementById('qtd');
-                qtd.style.display = "none";
-
-                setDatos(dado);
-                let dad = document.getElementById('dad');
-                dad.innerHTML = `<p>Carregando...</p>`;
-                await sleep(4000);
-                dad.innerHTML = ``;
-
-                dados.style.display = "block";
-
-                paginate.style.display = "block";
-
-                qtd.style.display = "block";
-            };
-            
-            console.log(dado);
-        } catch(error) {
-            console.log(error);
-
-            if( repos != '') { 
-                alert("Há algo de errado com a API. Recarregue a página. Se não funcionar tente em alguns minutos ou entre em contato com os desenvolvedores @erick_gbs ! Tente novamente em alguns minutos. "+error);
-            };
-        }
-        
-    };
-
-    useEffect(() => {
-        getdados();
-    },[pag]);
-
-    var last = paginas().length - 1
-
-    function paginas() {
-
-        let pages = [];
-        
-        let a = 0;
-        pages.unshift(
-            <a href="#header" key={a.toString()} >
-                <button onClick={ first_page } id='0'>First</button>
-            </a>
-        );
-        for (let i = 1; i < (qtd/10); i++){
-            pages.push(
-            <a href="#header" key={i.toString()}>
-                <button  id={i.toString()} onClick={any_page}>{i}</button>
-            </a>
-            );
-            
-            
-        };
-        pages.push(
-            <a href="#header" key={last_page}>
-                <button onClick={ last_page } id={last}>Last</button>
-            </a>
-        );
-        return pages;
-       
-    };
-
-    function first_page() {
-        setPag(1);
-        setPage_Range_First(1);
-        setPage_Range_End(7);
-    };
-    
-    function last_page() {
-        
-        setPag(last);
-        setPage_Range_First(paginas().length - 10);
-        setPage_Range_End(paginas().length - 2);
-        return last;
-    };
-
-    
-    
 return (
     <div className='search'>
         <form onSubmit={(e) => {e.preventDefault()}}>
